1b4c61a20b21bc3224ac4fdf1d6bcd9ea735b3cb
SivaSamudrala369 <sivagudise9@gmail.com>
2025-01-22 14:24:17 +0530

    Add files via upload

---
+++ Withdrawl/Withdrawl/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Withdraw
+{
+    public class Class1
+    {
+        public void Siva()
+        {
+            Console.WriteLine("Siva Withdraw 2000k");
+        }
+
+    }
+}
