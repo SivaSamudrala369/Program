7df65cf83fe41df1d8f19dd9490f4c21682da28a
SivaSamudrala369 <sivagudise9@gmail.com>
2025-01-22 14:33:29 +0530

    Add files via upload

---
+++ Deposite/Deposite/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deposite
+{
+    public class Class1
+    {
+        public void Ravi()
+        {
+            Console.WriteLine("Ravi Deposited 5000k");
+        }
+    }
+}
