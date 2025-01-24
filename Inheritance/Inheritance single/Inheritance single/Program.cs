c75c754fd96fa1680dbad806ad79da9137a0105a
SivaSamudrala369 <sivagudise9@gmail.com>
2025-01-24 18:04:49 +0530

    Add files via upload

---
+++ Inheritance/Inheritance single/Inheritance single/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_single
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+    }
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("BArking");
+        }
+        static void Main(string[] args)
+        {
+            Dog siva = new Dog();
+            siva.Bark();
+            siva.Eat();
+
+        }
+
+    }
+}
