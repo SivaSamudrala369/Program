6b61bcd485de932797e0a0f90dd065f3c21f938e
SivaSamudrala369 <sivagudise9@gmail.com>
2025-01-24 19:30:59 +0530

    Add files via upload

---
+++ Hierarchical Inheritance/Hierarchical Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HierarchicalInheritance
+{
+    public class ARMY
+    {
+        public void Siva()
+        {
+            Console.WriteLine("Failure");
+        }
+        public class POLICE : ARMY
+        {
+            public void Naresh()
+            {
+                Console.WriteLine("Success");
+            }
+        }
+        public class IPS : ARMY
+        {
+            public void Ravi()
+            {
+                Console.WriteLine("Well Position");
+            }
+        }
+        public class DGP : ARMY
+        {
+            public void Teja() 
+            {
+                Console.WriteLine("Retired");
+            }
+            static void Main(string[] args)
+            {
+                POLICE obj1 = new POLICE();
+                IPS obj2 = new IPS();
+                DGP obj3 = new DGP();
+                obj1.Siva();
+                obj1.Naresh();
+                obj2.Ravi();
+                obj3.Teja();
+
+            }
+        }
+    }
+}
+//output:Failure,Success,Well Position,Retired
