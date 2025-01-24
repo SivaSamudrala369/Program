04a8183d672771ef038c4bc9c10f736b588c34d5
SivaSamudrala369 <sivagudise9@gmail.com>
2025-01-24 18:33:57 +0530

    Add files via upload

---
+++ InhertanceMultiLevel/InhertanceMultiLevel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InhertanceMultiLevel
+{
+    public class Kurnool
+    {
+        public void Siva()
+        {
+            Console.WriteLine("Developer");
+        }
+        static void Main(string[] args)
+        {
+            Aspari mass = new Aspari();
+            mass.Ravi();
+            mass.Siva();
+            mass.Naresh();
+        }
+    }
+    public class Kakinada : Kurnool
+    {
+        public void Ravi()
+        {
+            Console.WriteLine("Wipro Developer");
+        }
+    }
+    public class Aspari : Kakinada
+    {
+        public void Naresh()
+        {
+            Console.WriteLine("SoftWare Engineer");
+        }
+    }
+    
+}
+// Object creats only child class
+//We can place Main Method in Any Class
